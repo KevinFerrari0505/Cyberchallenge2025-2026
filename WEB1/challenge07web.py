92baa234c963dd3b24c59f1126047531e72f3e6e
Kevin_Ferrari <ferrarikevin2005@gmail.com>
2026-04-01 23:38:18 +0200

    Delete WEB1 directory

--- WEB1/challenge07web.py
+++
@@ -1,4 +0,0 @@
-import requests
-
-r = requests.head('http://web-07.challs.olicyber.it/', allow_redirects = True)
-print(r.headers)
--- WEB1/challenge09web.py
+++
@@ -1,7 +0,0 @@
-import requests
-import json
-
-payload = {'username': 'admin', 'password': 'admin'}
-url = 'http://web-09.challs.olicyber.it/login'
-r = requests.post(url, json = payload)
-print(r.text)
