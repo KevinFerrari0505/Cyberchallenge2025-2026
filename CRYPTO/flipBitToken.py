4be006a2da0fb350d8910ccf61d083b4be347631
KevinFerrari0505 <ferrarikevin2005@gmail.com>
2026-04-16 00:51:28 +0200

    Delete CRYPTO

--- CRYPTO/flipBitToken.py
+++
@@ -1,9 +0,0 @@
-token = "2c55cd6af6f6381e06643932ed916592635e2f86a0246a119918a5705b18dba367fdcb6f6216dfdf61046c576dcda513" #cambia ogni volta
-data = bytearray.fromhex(token)
-
-# posizione dello '0' nel primo blocco
-pos = 15  
-
-data[pos] ^= 1
-
-print(data.hex())
