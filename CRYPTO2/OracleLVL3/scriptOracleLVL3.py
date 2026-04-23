57c8368bad24c60e5d83c6e0d66b3457b56b5b90
KevinFerrari0505 <ferrarikevin2005@gmail.com>
2026-04-23 18:57:37 +0200

    Modificato script e aggiunto la flag

--- CRYPTO2/OracleLVL3/scriptOracleLVL3.py
+++ CRYPTO2/OracleLVL3/scriptOracleLVL3.py
@@ -1,59 +1,41 @@
 from pwn import *
-from Crypto.Util.number import long_to_bytes
-from math import gcd
-import random
+from Crypto.Util.number import long_to_bytes, GCD, size
+from decimal import getcontext
 
-io = remote('oracle.challs.cyberchallenge.it', 9043)
+conn = remote("oracle.challs.cyberchallenge.it", 9043)
 
-io.recvuntil(b'Encrypted flag: ')
-C = int(io.recvline().strip())
-print(f"[+] flag_encrypted = {C}")
+def getFlag():
+    msg = conn.recvuntil(b"Encrypted flag: ")
+    return int(conn.recvline())
+def getToInputLine():
+    msg = conn.recvuntil(b'> ')
+    return msg
+def encrypt(msg):
+    getToInputLine()
+    conn.sendline(b"1")
+    getToInputLine()
+    conn.sendline(msg.encode())
+    conn.recvuntil(b"\nEncrypted: ")
+    return int(conn.recvline())
+def decrypt(msg):
+    getToInputLine()
+    conn.sendline(b"2")
+    getToInputLine()
+    conn.sendline(msg.encode())
+    conn.recvuntil(b"\nDecrypted: ")
+    return int(conn.recvline())
+def findClearFlag():
+    flag = getFlag()
+    e = 65537
+    numbers = [191, 197]
+    diffs = []
+    for number in numbers:
+        cipher = encrypt(str(number))
+        diffs.append(number**e - cipher)
+    n = GCD(diffs[0], diffs[1])
+    print(size(n))
+    flag = decrypt(str(-flag))
+    flag = (-1 * (flag)) % n
+    return long_to_bytes(flag)
 
-e = 65537
-
-def encrypt_val(val):
-    io.recvuntil(b'> ')
-    io.sendline(b'1')
-    io.recvuntil(b'Plaintext > ')
-    io.sendline(str(val).encode())
-    io.recvuntil(b'Encrypted: ')
-    return int(io.recvline().strip())
-
-def decrypt_val(val):
-    io.recvuntil(b'> ')
-    io.sendline(b'2')
-    io.recvuntil(b'Ciphertext > ')
-    io.sendline(str(val).encode())
-    io.recvuntil(b'Decrypted: ')
-    return int(io.recvline().strip())
-
-# ── Step 1: Modulus Recovery ──────────────────────────────────────────
-print("[*] Recovering N...")
-
-x, y = 2, 3
-enc_x = encrypt_val(x)
-enc_y = encrypt_val(y)
-
-x_e = pow(x, e)   # 2^65537 esatto
-y_e = pow(y, e)   # 3^65537 esatto
-
-n = gcd(x_e - enc_x, y_e - enc_y)
-
-print(f"[+] N = {n}")
-print(f"[+] N bits = {n.bit_length()}")
-
-# ── Step 2: Homomorphic Property  ───────────────────────────
-# Scelgo x=2, calcolo x^e mod n
-x = 2
-x_e = pow(x, e, n) #cifrato
-
-# Manda x^e * C al server
-C_blind = (x_e * C) 
-m_blind = decrypt_val(C_blind)   # = x * flag mod n
-
-# Recupero flag dividendo per x
-# flag = m_blind / x mod n  =  m_blind * x^(-1) mod n
-flag_int = (m_blind * pow(x, -1, n)) % n
-print(f"[+] Flag: {long_to_bytes(flag_int).decode()}")
-
-io.close()
+print(findClearFlag())
